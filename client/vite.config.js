f066245b5e85b36c1053cdbf338bddf2f90c9f89
Aman37773 <kumaraman88739@gmail.com>
2024-08-26 02:13:12 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:'/',
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:3001', // Adjust the port to match your server
+//     },
+//   },
+// })
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  base: '/',
   server: {
     proxy: {
-      '/api': 'http://localhost:3001', // Adjust the port to match your server
+      '/api': 'http://localhost:3001',
     },
   },
-})
-
-
-
+  build: {
+    outDir: 'dist', // Default build directory; copying to 'dest' with postbuild script
+  },
+});
